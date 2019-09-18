1e1ff85f208537c71f2871707de871f0c07507c1
领琾 <github@lingjye.com>
2019-09-19 00:35:26 +0800

    1

---
+++ flutter_shuqi-master/lib/app/app_scene.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shuqi/public.dart';
+import 'package:shuqi/app/root_scene.dart';
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+class AppScene extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '书旗小说',
+      navigatorObservers: [routeObserver],
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        dividerColor: Color(0xffeeeeee),
+        scaffoldBackgroundColor: SQColor.paper,
+        textTheme: TextTheme(body1: TextStyle(color: SQColor.darkGray)),
+      ),
+      home: RootScene(),
+    );
+  }
+}
